9faa4867150151191a3edf27b30a70c23563ac6b
Farhad16 <fhossain389@gmail.com>
2023-10-02 21:41:58 +0600

    Navbar is complete

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,3 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
-  return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
-      button
-    </div>
-  );
+  return <div>Homepage</div>;
 }
