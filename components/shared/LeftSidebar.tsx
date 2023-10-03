c9974ad70791bb101d455bc4efe9ec7e1bd283ee
Farhad16 <fhossain389@gmail.com>
2023-10-03 11:01:54 +0600

    Branch is created

---
+++ components/shared/LeftSidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LeftSidebar = () => {
+  return (
+    <section className="flex flex-col items-center justify-between pt-36 light-border border-r background-light900_dark200 sticky left-0 top-0 h-screen max-sm:hidden lg:w-[266px] shadow-light-300 dark:shadow-none overflow-y-auto">
+      sidebar
+    </section>
+  );
+};
+
+export default LeftSidebar;
