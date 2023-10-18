ad96210aa977edc74fa375e65dcf1c88cbb62313
Farhad16 <fhossain389@gmail.com>
2023-10-18 22:10:22 +0600

    Tags section done

--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -10,7 +10,7 @@ export default async function Page() {
 
   return (
     <>
-      <h1 className="h1-bold text-dark100_light900">Community</h1>
+      <h1 className="h1-bold text-dark100_light900">All Users</h1>
       <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
         <LocalSearch
           route="/community"
--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -2,7 +2,8 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
-import { GetTopInteractedTagsParams } from "./shared";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared";
+import Tag from "@/database/tag.model";
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
@@ -20,6 +21,19 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
       { _id: "2", name: "CSS" },
       { _id: "3", name: "JavaScript" },
     ];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase();
+
+    const tags = await Tag.find({});
+
+    return { tags };
   } catch (error) {
     console.log(error);
     throw error;
